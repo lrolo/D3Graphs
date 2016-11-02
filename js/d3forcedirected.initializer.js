5df5401450bf08a3e6a11652c894a6ca17216ad8
lrolo <luisrolo.tec@gmail.com>
2016-11-02 18:54:07 +0000

    latest changes

--- js/d3forcedirected.initializer.js
+++ js/d3forcedirected.initializer.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
         if (error) console.warn(error);
 
         var root = json;
+        root.fixed = true;
 
         var width = 960,
             height = 1000;
@@ -17,6 +18,7 @@ $(document).ready(function(){
             .attr('height', height);
 
         var graph = root;
+        graph.fixed = true;
 
         var color = d3.scaleOrdinal(d3.schemeCategory20);
 
@@ -29,18 +31,40 @@ $(document).ready(function(){
         var simulation = d3.forceSimulation()
             .force('link', d3.forceLink().id(function(d) {
                 return d.Identifier;
-            }).distance (10))
+            }).distance (20))
             .force('charge', d3.forceManyBody().strength(-width * 2))
             .force('center', d3.forceCenter(width / 2, height / 2))
             .force("collide", d3.forceCollide(20))
             .force('y', d3.forceY())
             .force('x', d3.forceX());
 
+        svg.append("defs").selectAll("marker")
+            .data(["end"])      // Different link/path types can be defined here
+            .enter().append("marker")    // This section adds in the arrows
+            .attr("id", String)
+            .attr("viewBox", "0 -5 10 10")
+            .attr("refX", (img_w + img_h / 2) - 10)
+            .attr("refY", -1.5)
+            .attr("markerWidth", 6)
+            .attr("markerHeight", 6)
+            .attr("orient", "auto")
+            .append("path")
+            .attr("d", "M0,-5L10,0L0,5");
+
+        /*
         var link = svg.append('g')
             .attr('class', 'links')
             .selectAll('line')
             .data(graph.links)
             .enter().append('line');
+        */
+
+        var link = svg.append('g')
+            .attr('class', 'links')
+            .selectAll('line')
+            .data(graph.links)
+            .enter().append('line')
+            .attr("marker-end", "url(#end)")
 
         var tip = d3.tip()
             .attr('class', 'd3-tip')
@@ -50,35 +74,35 @@ $(document).ready(function(){
                 var attributeList = '';
 
                 if (d.AttributeList.hasOwnProperty('MachineType')){
-                    attributeList += '<p>Machine type: '+ d.AttributeList.MachineType +'</p>';
+                    attributeList += '<p><span>Machine type: </span>'+ d.AttributeList.MachineType +'</p>';
                 }
 
                 if (d.AttributeList.hasOwnProperty('ActivationCode')){
-                    attributeList += '<p>Activation Code: '+ d.AttributeList.ActivationCode + '</p>';
+                    attributeList += '<p><span>Activation Code: </span>'+ d.AttributeList.ActivationCode + '</p>';
                 }
 
                 if (d.AttributeList.hasOwnProperty('InternalHostname')){
-                    attributeList += '<p>Internal Hostname: '+ d.AttributeList.InternalHostname+'</p>';
+                    attributeList += '<p><span>Internal Hostname: </span>'+ d.AttributeList.InternalHostname+'</p>';
                 }
 
                 if (d.AttributeList.hasOwnProperty('LifecycleState')){
-                    attributeList += '<p>Lifecycle State: '+ d.AttributeList.LifecycleState+'</p>';
+                    attributeList += '<p><span>Lifecycle State: </span>'+ d.AttributeList.LifecycleState+'</p>';
                 }
 
                 if (d.AttributeList.hasOwnProperty('Serial')){
-                    attributeList += '<p>Serial: '+ d.AttributeList.Serial+'</p>';
+                    attributeList += '<p><span>Serial: </span>'+ d.AttributeList.Serial+'</p>';
                 }
 
                 if (d.AttributeList.hasOwnProperty('EnvironmentType')){
-                    attributeList += '<p>Environment: '+ d.AttributeList.EnvironmentType+'</p>';
+                    attributeList += '<p><span>Environment: </span>'+ d.AttributeList.EnvironmentType+'</p>';
                 }
 
                 if (d.AttributeList.hasOwnProperty('Arn')){
-                    attributeList += '<p>Arn: '+ d.AttributeList.Arn+'</p>';
+                    attributeList += '<p><span>Arn: </span>'+ d.AttributeList.Arn+'</p>';
                 }
 
                 if (d.AttributeList.hasOwnProperty('ServerRole')){
-                    attributeList += '<p>Server: '+ d.AttributeList.ServerRole+'</p>';
+                    attributeList += '<p><span>Server: </span>'+ d.AttributeList.ServerRole+'</p>';
                 }
 
                 return '<div>' +
@@ -107,19 +131,18 @@ $(document).ready(function(){
             .on('mouseover', tip.show)
             .on('mouseout', tip.hide);
 
-        /*
+
         var nodes_text = svg.selectAll('.nodetext')
             .data(graph.nodes)
             .enter()
             .append('text')
             .attr('class', 'nodetext slds-text-heading--label')
             .attr('text-anchor', 'middle')
-            .attr('dx', -20)
-            .attr('dy', 10)
+            .attr('dx', -10)
+            .attr('dy', -5)
             .text(function(d) {
-                return d.Name;
+                return d.Type;
             });
-        */
 
     simulation
         .nodes(graph.nodes)
@@ -177,15 +200,15 @@ $(document).ready(function(){
                 return yPos;
             });
 
-        /*
+
         nodes_text
             .attr('x', function(d) {
-                return d.x + 20
+                return d.x + 10;
             })
             .attr('y', function(d) {
-                return d.y + img_w / 2;
+                return d.y + 20;
             });
-        */
+
         }
 
         function dragstarted(d) {
@@ -203,6 +226,24 @@ $(document).ready(function(){
             if (!d3.event.active) simulation.alphaTarget(0);
             d.fx = null;
             d.fy = null;
+        }
+
+        // Use elliptical arc path segments to doubly-encode directionality.
+        function tick() {
+          link.attr("d", linkArc);
+          node.attr("transform", transform);
+          nodes_text.attr("transform", transform);
+        }
+
+        function linkArc(d) {
+          var dx = d.target.x - d.source.x,
+              dy = d.target.y - d.source.y,
+              dr = Math.sqrt(dx * dx + dy * dy);
+          return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+        }
+
+        function transform(d) {
+          return "translate(" + d.x + "," + d.y + ")";
         }
 
     });
